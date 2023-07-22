7998d88a726c5792e898a4c641bbd5129e76c02e
Tony <antonivelasquez67@gmail.com>
2023-07-22 12:59:02 -0400

    Finalized CSS, js tags needs changes

--- calculator.js
+++ calculator.js
@@ -1,25 +1,22 @@
-function howManyday(month) {
-  var days = [31, 30, 28];
-  switch (month) {
-    case 2:
-      return days[2];
-      break;
-    case 4:
-    case 6:
-    case 9:
-    case 11:
-      return days[1];
-      break;
-    case 1:
-    case 3:
-    case 5:
-    case 7:
-    case 8:
-    case 10:
-    case 12:
-      return days[0];
+const add = (...args) => args.reduce((sum, total) => sum + total, 0);
+
+const multiplication = (...args) =>
+  args.reduce((multi, total) => multi * total, 1);
+
+const subtraction = (...args) =>
+  args.reduce((subtract, current) => subtract - current);
+
+function Divide(divident, divisor) {
+  let result = divident / divisor;
+
+  if (result < 0) {
+    // Rounds the number if it is negative
+    return Math.ceil(result);
   }
-  return days;
+
+  return Math.floor(result);
 }
 
-howManyday();
+const num1 = document.querySelectorAll(".keys");
+let num2 = document.querySelectorAll(".keys");
+let operator = document.querySelectorAll(".operators");
