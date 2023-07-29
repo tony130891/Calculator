cb229bd2b6190a4e9a13365bccb182715b57b9b2
Tony <antonivelasquez67@gmail.com>
2023-07-28 21:58:55 -0400

    Finally made the clear button work

--- calculator.js
+++ calculator.js
@@ -1,14 +1,23 @@
 const displayer = document.querySelector("#inputBox");
 const btn = document.querySelectorAll(".button");
 const btnOp = document.querySelectorAll("button.operator");
-const btnEqual = document.querySelectorAll("button.equal");
+const btnEqual = document.querySelector("button.equal");
+const btnDeleter = document.querySelector("button.delete");
 let string = "";
 
+const clearContent = () => {
+  string = "";
+  return (displayer.value = string);
+};
 btn.forEach((number) => {
   number.addEventListener("click", function (e) {
-      // check if the event works correctly on the target
+    // check if the event works correctly on the target
     console.log(e.target);
     string += e.target.textContent;
     displayer.value = string;
+
+    if (number === btnDeleter) {
+      clearContent();
+    }
   });
 });
